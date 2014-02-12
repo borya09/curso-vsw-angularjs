ec6338c792b968a294b2e61c854879298f20acc9
borja andrés <borya09@gmail.com>
2014-02-12 10:44:50 +0100

    directive uptaded

--- app/scripts/mis-gastos-directives.js
+++ app/scripts/mis-gastos-directives.js
@@ -3,9 +3,19 @@ app.directive('pruebaSaludo', function () {
         restrict: 'E',
         templateUrl: 'views/prueba-directiva.html',
         replace: true,
+        scope:{
+        },
         link: function (scope, elem, attrs) {
             scope.de = attrs.de;
             scope.a = attrs.a;
+
+            $(elem).on('click', function(){
+                var tmp = scope.de;
+                scope.de = scope.a;
+                scope.a = tmp;
+                scope.$apply();
+            });
+
         }
     }
 });
